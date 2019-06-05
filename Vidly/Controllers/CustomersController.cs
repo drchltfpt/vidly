0951e8b92cc3a20b608e0a8c5ce82ec7a3791db1
Pham Huy Ha <ha.phamhuy@vietis.vn>
2019-06-05 17:16:57 +0700

    Init

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customers = new List<Customer> {
+            new Customer { Id = 1, Name = "John Smith" },
+            new Customer { Id = 2, Name = "Mary Williams" }
+        };
+        //
+        // GET: /Customers/
+        public ActionResult Index()
+        {
+
+            var viewModel = new CustomerViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            foreach (var customer in customers)
+            {
+                if (customer.Id == id)
+                {
+                    return View(customer);
+                }
+            }
+            return HttpNotFound();
+        }
+    }
+}
---
+++ Vidly/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly.Startup))]
+namespace Vidly
+{
+    public partial class Startup
+    {
+        ConfigureAuth(app);
+    }
+}
