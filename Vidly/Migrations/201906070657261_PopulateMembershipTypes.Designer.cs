124453abaecf7107d3e1215a705f71d3ddb40320
Pham Huy Ha <ha.phamhuy@vietis.vn>
2019-06-07 14:19:51 +0700

    Section 1

--- Vidly/Migrations/201906070657261_PopulateMembershipTypes.Designer.cs
+++ Vidly/Migrations/201906070657261_PopulateMembershipTypes.Designer.cs
@@ -7,13 +7,13 @@ namespace Vidly.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class InitialModel : IMigrationMetadata
+    public sealed partial class PopulateMembershipTypes : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialModel));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(PopulateMembershipTypes));
         
         string IMigrationMetadata.Id
         {
-            get { return "201906050850040_InitialModel"; }
+            get { return "201906070657261_PopulateMembershipTypes"; }
         }
         
         string IMigrationMetadata.Source
---
+++ Vidly/VidlyContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Vidly.Models;
+
+namespace Vidly
+{
+    public class VidlyContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public VidlyContext()
+        {
+        }
+    }
+}
